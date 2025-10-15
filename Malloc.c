5442ca277f2bfff001f4e3822e39b96e324ad0cb
Hasib <gudhulibela031@gmail.com>
2025-10-15 23:57:53 +0600

    Add files via upload

---
+++ Malloc.c
@@ -0,0 +1,16 @@
+//malloc
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+   int *ptr;
+   ptr=(int *)malloc(5*sizeof(int));
+   if(ptr==NULL)
+   {
+       printf("Memory not allocated");
+   }
+   else
+   {
+       printf("\nMemory allocated");
+   }
+}
